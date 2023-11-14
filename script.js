5687491e0f820c848f490f0f8825574a8cbcf7d5
Aaron0066 <132987781+Aaron0066@users.noreply.github.com>
2023-11-14 01:10:53 -0500

    deploy

---
+++ script.js
@@ -0,0 +1,58 @@
+let weather = {
+    apiKey: "60b9e1f6f930bc091153df32ff0088b0",
+    fetchWeather: function(city) {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            this.displayWeather(data);
+        })
+        .catch((error) => {
+            console.error("Error fetching weather data:", error);
+        });
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log("Weather data:", data);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function () {
+       this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+weather.fetchWeather("Charlotte");
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+
+});
+
+weather.fetchWeather("Charlotte");
+
+
+
+
+
+
+
+
+
